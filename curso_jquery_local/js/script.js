d2ce12d150216c6b624a7df6912e236ff0d861b5
dev14 <dev14@dev14.localdomain>
2018-09-26 11:44:10 -0300

    aula 03 curso android

---
+++ curso_jquery_local/js/script.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+    
+    //De um por um 
+    /*$('h1').css('color','#fff');
+    $('h1').hide();
+    $('h1').fadeIn(1000);
+    $('h1').fadeIn('fast');
+    $('h1').css('color','red');
+    
+    
+    //Inline
+    $('h1').css('color','red').hide().fadeIn(1000).fadeIn('fast');
+    
+    //Blocks
+    $('h1')
+        .css('color','red')
+        .hide()
+        .fadeIn(1000)
+        .fadeIn('slow');*/
+    
+    $('a').css('color','#ff0');
+    $('#link2').css('color','red');
+    $('.link3').css('color','green');
+    
+});
